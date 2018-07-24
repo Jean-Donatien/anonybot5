19494c6245f54c0276877bbede168e617afee91c
Jean-Donatien <38259101+Jean-Donatien@users.noreply.github.com>
2018-07-24 12:56:55 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-
+var channel = client.guilds.get("361975566120779778").channels.get("471245436603072514");
 
 client.on("message", message => {
 if (message.content === "dzq") {
   //client.channels.get("<471245436603072514>").send(message.content);
-  client.guilds.get("361975566120779778").channels.get("471245436603072514").send("Ce message m'a été envoyé:"+message.content);
-  //message.channel.send("Saucisse"+message.content);
+  //client.guilds.get("361975566120779778").channels.get("471245436603072514").send("Ce message m'a été envoyé:"+message.content);
+  message.channel.send("Saucisse"+message.content);
   //message.reply(message.content);
   }
 });
