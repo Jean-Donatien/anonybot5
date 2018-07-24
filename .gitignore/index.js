d1cb7c62feb934007e5e3038b3ef5fd2afd47ae4
Jean-Donatien <38259101+Jean-Donatien@users.noreply.github.com>
2018-07-24 15:22:08 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,11 +4,11 @@ const client = new Discord.Client();
 //var channel = client.guilds.get("361975566120779778").channels.get("471245436603072514");
 
 client.on("message", message => {
-if (message.content === "dzq") {
+if (message.content.startsWith("anon: ")) {
   //client.channels.get("<471245436603072514>").send(message.content);
   //client.guilds.get("361975566120779778").channels.get("471245436603072514").send("Ce message m'a été envoyé:"+message.content);
   message.delete()
-  message.channel.send("Message-anonyme: "+message.content);
+  message.channel.send("Message-anonyme: "+message.content.slice(6, message.content.length));
   //message.reply(message.content);
   }
 });
