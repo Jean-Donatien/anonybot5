36cf1a6637b1962070fc6f1bf0a4cc9bd2a2e9a9
Jean-Donatien <38259101+Jean-Donatien@users.noreply.github.com>
2018-07-24 15:11:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ if (message.content === "dzq") {
   //client.channels.get("<471245436603072514>").send(message.content);
   //client.guilds.get("361975566120779778").channels.get("471245436603072514").send("Ce message m'a été envoyé:"+message.content);
   message.delete()
-  message.channel.send("Anonyme: "+message.content);
+  message.channel.send("Message-anonyme: "+message.content);
   //message.reply(message.content);
   }
 });
