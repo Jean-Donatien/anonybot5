93c8e61776d789f1dc32ab2d7168fc6d082f3cbd
Jean-Donatien <38259101+Jean-Donatien@users.noreply.github.com>
2018-07-24 15:09:58 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -7,6 +7,7 @@ client.on("message", message => {
 if (message.content === "dzq") {
   //client.channels.get("<471245436603072514>").send(message.content);
   //client.guilds.get("361975566120779778").channels.get("471245436603072514").send("Ce message m'a été envoyé:"+message.content);
+  message.delete()
   message.channel.send("Anonyme: "+message.content);
   //message.reply(message.content);
   }
