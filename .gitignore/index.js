cd1c11dd7f85e9cbd91201f95a2dd654e299c0b9
Jean-Donatien <38259101+Jean-Donatien@users.noreply.github.com>
2018-07-24 11:49:35 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on("message", message => {
+if (message.startwith === "ping") {
+  client.channels.get("<471245436603072514>").send(message);
+  }
+});
+
+client.login('process.env.TOKEN');
