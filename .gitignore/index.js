f88c800333f198afb9a01063d1ee91dc412a747a
Jean-Donatien <38259101+Jean-Donatien@users.noreply.github.com>
2019-02-23 15:31:17 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -17,11 +17,11 @@ client.on("message", message => {
   }
 });
 
-client.on("message", message => {
+/*client.on("message", message => {
   var fileSystem=new ActiveXObject("Scripting.FileSystemObject");
   var monfichier=fileSystem.OpenTextFile("messageEnvoye.txt", 2 ,true);
   monfichier.WriteLine(message);
-});
+});*/
 
 
 client.login(process.env.TOKEN);
